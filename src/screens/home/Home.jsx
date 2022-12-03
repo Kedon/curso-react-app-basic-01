79e23ade2dc384574b831053523e5779290802b6
Kedon <valdenir@kedon.com.br>
2022-12-03 13:23:00 -0300

    Projeto Git

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,61 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './home.scss';
-import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
-import Card from 'react-bootstrap/Card';
+import { Button, Table, Card, Form, Modal } from 'react-bootstrap'
+
 
 const Home = () => {
 
+    const [modal, showModal] = useState(false)
+
+    const [fields, mudaCampo] = useState({
+        name: '',
+        lastName: '',
+        userName: ''
+    })
+
+    const [rows, setRows] = useState([])
+
+    function toggleModal(){
+        showModal(!modal)
+    }
+
+    function changeInputField(e) {
+        console.log(e)
+        const { name, value } = e.target
+        console.log(name, value)
+        mudaCampo({
+            ...fields,
+            [name]: value
+        })
+    }
+
+    function handleAddField() {
+        console.log(fields)
+    }
+
     return (
         <div className="home-layout">
-            <div className="rixon">
-                <div className="joao-vitor">
-                    <ul>
-                        <li className="active">
-                            <a href="/home">Valdenir</a>
-                        </li>
-                        <li><a href="/products">Flauzino</a></li>
-                        <li><a href="/react">Valdenir</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div className="celular">
-                <div className="iphone">
-                    <h1>Iphone 14</h1>
-                    <p>Paragrafo</p>
-                </div>
-            </div>
+            <Modal show={modal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal title</Modal.Title>
+                </Modal.Header>
 
+                <Modal.Body>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Nome</Form.Label>
+                        <Form.Control name="name" type="text" value={fields.name} placeholder="Seu nome" onChange={changeInputField} />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Sobrenome</Form.Label>
+                        <Form.Control name="lastName" type="text" value={fields.lastName} placeholder="Sobrenome" onChange={changeInputField} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>User name</Form.Label>
+                        <Form.Control name="userName" type="text" value={fields.userName} placeholder="Nome de usuário" onChange={changeInputField} />
+                    </Form.Group>
+
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={toggleModal}>Close</Button>
+                    <Button variant="primary"  onClick={handleAddField}>Save changes</Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Button variant="primary" onClick={toggleModal}>
+                Abrir modal
+            </Button>
             <Table striped bordered hover className="mt-4">
                 <thead>
                     <tr>
                         <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
+                        <th>Nome</th>
+                        <th>Sobrenome</th>
+                        <th>Nome de Usuário</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
+                        <td>Valdenir</td>
+                        <td>Flauzino</td>
+                        <td>Val</td>
                     </tr>
                 </tbody>
             </Table>
-            <div className="content">
+            {/* <div className="content">
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src={require('../../assets/img/logo-og.png')} />
                     <Card.Body>
@@ -67,8 +100,8 @@ const Home = () => {
                         <Button variant="primary">Go somewhere</Button>
                     </Card.Body>
                 </Card>
-            </div>
-            <Button variant="primary">Primary</Button>{' '}
+            </div> */}
+            {/* <Button variant="primary">Primary</Button>{' '} */}
         </div>
     )
 };
